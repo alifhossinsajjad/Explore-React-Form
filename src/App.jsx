926d555ec3de14f6bbf3cd015b4b895c0d9e4b01
MD ALIF MIA <alifhossinsajjad123456@gmail.com>
2025-09-29 22:17:17 +0600

    explore react form

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import SimpleForm from './Components/SimpleForm/SimpleForm'
+
+function App() {
+ 
+
+  return (
+    <>
+ 
+      <h1>Eplore Form</h1>
+     <SimpleForm/>
+    </>
+  )
+}
+
+export default App
